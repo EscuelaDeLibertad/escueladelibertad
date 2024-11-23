f92285b6ac688548c90ae80a14de5ee842365776
Esteban Alban <esteban.alban9003@gmail.com>
2024-11-22 22:39:03 -0500

    Se agrega a Erika como admin

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,6 +1,7 @@
 export const isTeacher = (userId?: string | null) => {
 	return (
 		userId === process.env.NEXT_PUBLIC_TEACHER_ID ||
-		userId === process.env.NEXT_PUBLIC_TEACHER_ID2
+		userId === process.env.NEXT_PUBLIC_TEACHER_ID2 ||
+		userId === process.env.NEXT_PUBLIC_TEACHER_ID3
 	);
 };
