d470a0800745a73259d3ad24f2d95c2726a1767c
Esteban <esteban.alban9003@gmail.com>
2025-02-14 22:31:46 -0500

    cambios a la hora de mostrar info del curso

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -10,107 +10,116 @@ import { FileIcon } from "lucide-react";
 import CourseProgressButton from "./_components/course-progress-button";
 
 const ChapterIdPage = async ({
-	params,
+  params,
 }: {
-	params: {
-		courseId: string;
-		chapterId: string;
-	};
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
 }) => {
-	const { userId } = auth();
+  const { userId } = auth();
 
-	if (!userId) {
-		return redirect("/");
-	}
+  if (!userId) {
+    return redirect("/");
+  }
 
-	const {
-		chapter,
-		course,
-		muxData,
-		attachments,
-		nextChapter,
-		userProgress,
-		purchase,
-	} = await getChapter({
-		userId,
-		chapterId: params.chapterId,
-		courseId: params.courseId,
-	});
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
 
-	if (!chapter || !course) {
-		return redirect("/");
-	}
+  if (!chapter || !course) {
+    return redirect("/");
+  }
 
-	const isLocked = !chapter.isFree && !purchase;
+  const isLocked = !chapter.isFree && !purchase;
 
-	const completeOnEnd = !!purchase && !userProgress?.isComplete;
+  const infoLocked = !chapter.isFree;
 
-	return (
-		<div>
-			{userProgress?.isComplete && (
-				<Banner variant="success" label="Ya has completado este capítulo" />
-			)}
-			{isLocked && (
-				<Banner
-					variant="warning"
-					label="Debes comprar el curso para acceder a este capitulo"
-				/>
-			)}
-			<div className="flex flex-col max-w-4xl mx-auto pb-20">
-				<div className="p-4">
-					<VideoPlayer
-						chapterId={chapter.id}
-						title={chapter.title}
-						courseId={params.courseId}
-						nextChapterId={nextChapter?.id}
-						playbackId={muxData?.playbackId!}
-						isLocked={isLocked}
-						completeOnEnd={completeOnEnd}
-					/>
-				</div>
-				<div>
-					<div className="p-4 flex flex-col md:flex-row items-center justify-between">
-						<h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
-						{purchase ? (
-							<CourseProgressButton
-								chapterId={params.chapterId}
-								courseId={params.courseId}
-								nextChapterId={nextChapter?.id}
-								isComplete={!!userProgress?.isComplete}
-							/>
-						) : (
-							<CourseEnrollButton
-								courseId={params.courseId}
-								price={course.price!}
-							/>
-						)}
-					</div>
-					<Separator />
-					<div>
-						<Preview value={chapter.description!} />
-					</div>
-					{!!attachments.length && (
-						<>
-							<Separator />
-							<div className="p-4">
-								{attachments.map((attachment) => (
-									<a
-										href={attachment.url}
-										key={attachment.id}
-										target="_blank"
-										className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:bg-sky-300 hover:text-sky-800"
-									>
-										<FileIcon className="h-4 w-4 mr-2" />
-										<p className="line-clamp-1">{attachment.name}</p>
-									</a>
-								))}
-							</div>
-						</>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  const completeOnEnd = !!purchase && !userProgress?.isComplete;
+
+  return (
+    <div>
+      {userProgress?.isComplete && (
+        <Banner variant="success" label="Ya has completado este capítulo" />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="Debes comprar el curso para acceder a este capitulo"
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={chapter.id}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+        <div>
+          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+            {purchase ? (
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isComplete={!!userProgress?.isComplete}
+              />
+            ) : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )}
+          </div>
+          <Separator />
+          <div>
+            {!infoLocked ? (
+              <Preview value={chapter.description!} />
+            ) : (
+              <p>
+                La información de este capítulo es exclusivo para estudiantes de
+                este curso
+              </p>
+            )}
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    key={attachment.id}
+                    target="_blank"
+                    className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:bg-sky-300 hover:text-sky-800"
+                  >
+                    <FileIcon className="h-4 w-4 mr-2" />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterIdPage;
