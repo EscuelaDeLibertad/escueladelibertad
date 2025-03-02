bfcee4a4f617cce885e90747b92001f43f78cf6f
Esteban <esteban.alban9003@gmail.com>
2025-03-02 13:28:05 -0500

    Cambios a boton de pago

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -7,36 +7,47 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
-	price: number;
-	courseId: string;
+  price: number;
+  courseId: string;
 }
 
 const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const isFull =
+    courseId === "3a465407-1ec5-4d8b-9a97-cd528d105f0e" ||
+    "c20f1e2a-2d11-4122-b193-577e0f6c7c4e";
 
-	const onClick = async () => {
-		try {
-			setIsLoading(true);
-			const response = await axios.post(`/api/courses/${courseId}/checkout`);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
 
-			window.location.assign(response.data.url);
-		} catch {
-			toast.error("Algo salio mal");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+      window.location.assign(response.data.url);
+    } catch {
+      toast.error("Algo salio mal");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-	return (
-		<Button
-			onClick={onClick}
-			disabled={isLoading}
-			className="w-full md:w-auto"
-			size="sm"
-		>
-			Compra el curso por {formatPrice(price)}
-		</Button>
-	);
+  return (
+    <>
+      {isFull ? (
+        <Button disabled className="w-full md:w-auto" size="sm">
+          Plazas Llenas
+        </Button>
+      ) : (
+        <Button
+          onClick={onClick}
+          disabled={isLoading}
+          className="w-full md:w-auto"
+          size="sm"
+        >
+          Compra el curso por {formatPrice(price)}
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default CourseEnrollButton;
